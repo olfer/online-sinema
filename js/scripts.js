73fa93bbde8d57b4591aa9d79904b4f7f7025563
olfer <72212563+olfer@users.noreply.github.com>
2021-10-11 20:44:09 +0300

    Добавилась адаптивность и видео по кнопке

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector('.scheme-svg');
 const totalPriceTag= document.querySelector('.price-total')
+const menuButton= document.querySelector('.m-menu')
+const menu= document.querySelector('.menu')
 let cost = 800;
 let totalPrice = 0;
 schemeSvg.addEventListener('click', (event) =>{
@@ -11,3 +13,7 @@ schemeSvg.addEventListener('click', (event) =>{
         
     }
 });
+menuButton.addEventListener('click', () => {
+    console.log('кликнули по меню')
+    menu.classList.toggle('is-open');
+})
